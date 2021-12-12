e111ca9ede25daf1f6093fd16258d1a0980b1bc9
BackRunner <dev@backrunner.top>
2021-12-12 23:21:17 +0800

    feat: add postfix slot

--- src/packages/input/AInput.vue
+++ src/packages/input/AInput.vue
@@ -5,6 +5,7 @@
       'a-input--large': size === 'large',
       'a-input--round': round,
       'a-input--has-prefix': hasPrefix,
+      'a-input--has-postfix': hasPostfix,
       'a-input--disabled': disabled,
       'a-input--readonly': readonly,
     }"
@@ -16,10 +17,12 @@
       :placeholder="placeholder"
       :disabled="disabled"
       :readonly="readonly"
+      autocomplete="off"
       @input="handleInput"
       @keydown.enter="handleEnterDown"
     />
     <div v-if="hasPrefix" class="a-input__prefix"><slot name="prefix"></slot></div>
+    <div v-if="hasPostfix" class="a-input__postfix"><slot name="postfix"></slot></div>
   </div>
 </template>
 
@@ -109,6 +112,7 @@ export default defineComponent({
     return {
       storedValue,
       hasPrefix: !!useSlots().prefix,
+      hasPostfix: !!useSlots().postfix,
       style: {
         width: formatStyleSize(props.width),
       },
@@ -121,7 +125,7 @@ export default defineComponent({
 
 <style lang="scss">
 .a-input {
-  height: 40px;
+  height: var(--comp-height--default);
   position: relative;
   box-sizing: border-box;
   &__inner {
@@ -150,16 +154,21 @@ export default defineComponent({
     padding: 4px 18px;
   }
 }
+
+@mixin input-inserted {
+  position: absolute;
+  top: 0;
+  line-height: var(--comp-height--default);
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+
 .a-input.a-input--has-prefix {
   .a-input__prefix {
-    position: absolute;
-    top: 0;
     left: 0;
-    line-height: 40px;
-    display: flex;
-    align-items: center;
-    height: 100%;
     padding-left: 8px;
+    @include input-inserted();
     svg {
       width: 22px;
       height: 22px;
@@ -172,8 +181,25 @@ export default defineComponent({
     padding-left: 32px;
   }
 }
+.a-input.a-input--has-postfix {
+  .a-input__postfix {
+    right: 0;
+    padding-right: 8px;
+    @include input-inserted();
+    svg {
+      width: 22px;
+      height: 22px;
+      path {
+        stroke: var(--placeholder);
+      }
+    }
+  }
+  .a-input__inner {
+    padding-right: 32px;
+  }
+}
 .a-input--large {
-  height: 48px;
+  height: var(--comp-height--large);
   .a-input__inner {
     line-height: 40px;
     font-size: 16px;
@@ -189,6 +215,7 @@ export default defineComponent({
 }
 .a-input.a-input--large.a-input--has-prefix {
   .a-input__prefix {
+    line-height: var(--comp-height--large);
     svg {
       width: 26px;
       height: 26px;
@@ -198,12 +225,32 @@ export default defineComponent({
     padding-left: 36px;
   }
 }
+.a-input.a-input--large.a-input--has-postfix {
+  .a-input__postfix {
+    line-height: var(--comp-height--large);
+    svg {
+      width: 26px;
+      height: 26px;
+    }
+  }
+  .a-input__inner {
+    padding-right: 36px;
+  }
+}
 .a-input.a-input--round.a-input--large.a-input--has-prefix {
   .a-input__prefix {
     padding-left: 12px;
   }
   .a-input__inner {
     padding-left: 40px;
+  }
+}
+.a-input.a-input--round.a-input--large.a-input--has-postfix {
+  .a-input__postfix {
+    padding-right: 12px;
+  }
+  .a-input__inner {
+    padding-right: 40px;
   }
 }
 .a-input.a-input--disabled {
--- src/testground/app.vue
+++ src/testground/app.vue
@@ -23,15 +23,14 @@
     <div class="testground-footer">
       <Logo class="logo" />
       <span class="text">Any Design</span>
-      <a href="https://github.com/any-design/anyui" target="_blank"
-        ><GitHubIcon class="github"
-      /></a>
+      <a href="https://github.com/any-design/anyui" target="_blank"><icon icon="mdi:github" /></a>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 import ButtonSection from './sections/button.vue';
 import ClickableTextSection from './sections/clickableText.vue';
 import GradientTextSection from './sections/gradientText.vue';
@@ -47,7 +46,6 @@ import PopMenuSection from './sections/popmenu.vue';
 import FormSection from './sections/form.vue';
 import MessageSection from './sections/message.vue';
 import Logo from './icon/Logo.vue';
-import GitHubIcon from './icon/GitHub.vue';
 
 export default defineComponent({
   components: {
@@ -66,7 +64,7 @@ export default defineComponent({
     FormSection,
     MessageSection,
     Logo,
-    GitHubIcon,
+    Icon,
   },
 });
 </script>
