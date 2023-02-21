a0a665e12c3adb381da3d3820c91416d261518b3
BackRunner <dev@backrunner.top>
2023-02-22 01:22:27 +0800

    feat: (testground) add checkbox group

--- src/testground/sections/checkbox.vue
+++ src/testground/sections/checkbox.vue
@@ -5,4 +5,16 @@
       <a-checkbox label="Checkbox"></a-checkbox>
     </div>
   </div>
+  <div class="testground-content__item">
+    <p class="testground-content-title">Checkbox Group</p>
+    <div class="testground-flex">
+      <a-checkbox-group :items="items"></a-checkbox-group>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const items = ref(['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5']);
+</script>
