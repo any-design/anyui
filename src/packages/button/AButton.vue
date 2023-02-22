83704c33c4eeb26e98578c40c6d1ba04ab1f537a
BackRunner <dev@backrunner.top>
2023-02-22 22:43:23 +0800

    breaking: (button) default size not will be smaller than before, and all icon button styles were optimized

--- src/packages/button/AButton.vue
+++ src/packages/button/AButton.vue
@@ -75,7 +75,7 @@ export default defineComponent({
   position: relative;
   height: 42px;
   line-height: 30px;
-  padding: 6px 24px;
+  padding: 4px 20px;
   background: var(--bg);
   color: var(--primary);
   border-radius: 4px;
@@ -99,19 +99,19 @@ export default defineComponent({
   border-radius: 24px;
 }
 .a-button.a-button--large {
-  font-size: 18px;
-  line-height: 40px;
-  padding: 8px 32px;
-  height: 56px;
+  font-size: 16px;
+  line-height: 36px;
+  padding: 6px 24px;
+  height: 52px;
 }
 .a-button.a-button--large.a-button--round {
   border-radius: 30px;
 }
 .a-button.a-button--small {
-  font-size: 13px;
+  font-size: 12px;
   line-height: 28px;
-  padding: 4px 20px;
-  height: 36px;
+  padding: 3px 14px;
+  height: 32px;
 }
 .a-button.a-button--small.a-button--round {
   border-radius: 18px;
@@ -122,19 +122,19 @@ export default defineComponent({
 .a-button.a-button--primary {
   background: var(--primary);
   color: var(--btn);
-  box-shadow: 0 4px 10px var(--shadow-20);
+  box-shadow: 0 3px 14px var(--shadow-18);
   border: none;
 }
 .a-button.a-button--gradient {
   background: linear-gradient(90deg, var(--primary), var(--secondary));
   color: var(--btn);
-  box-shadow: 0 4px 10px var(--shadow-20);
+  box-shadow: 0 3px 14px var(--shadow-18);
   border: none;
 }
 .a-button.a-button--gradient-reserve {
   background: linear-gradient(90deg, var(--secondary), var(--primary));
   color: var(--btn);
-  box-shadow: 0 4px 10px var(--shadow-20);
+  box-shadow: 0 3px 14px var(--shadow-18);
   border: none;
 }
 .a-button.a-button--primary:hover {
@@ -150,27 +150,86 @@ export default defineComponent({
 }
 .a-button.a-button--anim:hover {
   transform: translateY(-4px);
-  box-shadow: 0 4px 16px var(--shadow-25);
+  box-shadow: 0 4px 10px var(--shadow-24);
 }
 .a-button.a-button--disabled {
   transition: none;
   border: none;
   background: var(--disabled) !important;
   color: var(--text-disabled) !important;
-  box-shadow: 0 2px 10px var(--shadow-5);
+  box-shadow: 0 3px 12px var(--shadow-5);
   cursor: not-allowed;
 }
 .a-button--icon {
   display: flex;
   align-items: center;
+  padding: 6px 14px;
   svg {
     font-size: 1.2em;
     flex-shrink: 0;
   }
 }
+.a-button--small.a-button--icon {
+  padding: 4px 10px;
+  svg {
+    font-size: 1.125em;
+  }
+}
+.a-button--large.a-button--icon {
+  padding: 8px 18px;
+  svg {
+    font-size: 1.375em;
+  }
+}
+.a-button--round.a-button--icon {
+  padding: 6px 12px;
+  svg {
+    font-size: 1.375em;
+  }
+}
+.a-button--small.a-button--round.a-button--icon {
+  padding: 4px 8px;
+}
+.a-button--large.a-button--round.a-button--icon {
+  padding: 8px 16px;
+  svg {
+    font-size: 1.375em;
+  }
+}
 .a-button--icon-text {
+  padding: 6px 18px 6px 16px;
   svg {
     margin-right: 4px;
+  }
+}
+.a-button--small.a-button--icon-text {
+  padding: 4px 12px 4px 10px;
+  svg {
+    margin-right: 3px;
+  }
+}
+.a-button--large.a-button--icon-text {
+  padding: 8px 24px 8px 18px;
+  svg {
+    margin-right: 6px;
+  }
+}
+.a-button--round.a-button--icon-text {
+  padding: 6px 16px 6px 12px;
+  svg {
+    margin-right: 4px;
+  }
+}
+.a-button--round.a-button--small.a-button--icon-text {
+  padding: 4px 10px 4px 8px;
+  svg {
+    margin-right: 3px;
+  }
+}
+.a-button--round.a-button--large.a-button--icon-text {
+  padding: 8px 22px 8px 16px;
+  svg {
+    margin-right: 6px;
   }
 }
 </style>
--- src/testground/sections/button.vue
+++ src/testground/sections/button.vue
@@ -2,22 +2,38 @@
   <div class="testground-content__item">
     <p class="testground-content-title">Button</p>
     <div class="testground-flex">
+      <a-button size="small">Small</a-button>
       <a-button>Default</a-button>
+      <a-button size="large">Large</a-button>
       <a-button type="primary">Primary</a-button>
       <a-button type="primary" round>Round</a-button>
       <a-button type="primary" round anim>Anim</a-button>
+      <a-button size="small" round>Small Round</a-button>
       <a-button size="large" round>Large</a-button>
-      <a-button size="small" round>Small</a-button>
       <a-button type="primary" round disabled>Disabled</a-button>
     </div>
     <div class="testground-flex" style="margin-top: 20px">
+      <a-button type="primary" size="small" icon="eva:alert-circle-fill"></a-button>
       <a-button type="primary" icon="eva:alert-circle-fill"></a-button>
+      <a-button type="primary" size="large" icon="eva:alert-circle-fill"></a-button>
+      <a-button type="primary" size="small" icon="eva:alert-circle-fill" round></a-button>
+      <a-button type="primary" icon="eva:alert-circle-fill" round></a-button>
+      <a-button type="primary" size="large" icon="eva:alert-circle-fill" round></a-button>
+      <a-button type="primary" size="small" icon="eva:alert-circle-fill"
+        >Small Icon Button</a-button
+      >
       <a-button type="primary" icon="eva:alert-circle-fill">Icon Button</a-button>
       <a-button type="primary" size="large" icon="eva:alert-circle-fill"
         >Large Icon Button</a-button
       >
-      <a-button type="primary" size="small" icon="eva:alert-circle-fill"
+    </div>
+    <div class="testground-flex" style="margin-top: 20px">
+      <a-button type="primary" size="small" icon="eva:alert-circle-fill" round
         >Small Icon Button</a-button
+      >
+      <a-button type="primary" icon="eva:alert-circle-fill" round>Icon Button</a-button>
+      <a-button type="primary" size="large" icon="eva:alert-circle-fill" round
+        >Large Icon Button</a-button
       >
     </div>
   </div>
