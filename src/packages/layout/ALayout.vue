2cdf0dc8a0413ef5fa37e776bfe2a797680d8d20
BackRunner <dev@backrunner.top>
2023-11-14 02:42:16 +0800

    feat: (layout) now layout will fill the parent element when the parent element has relative position setting

--- src/packages/layout/ALayout.vue
+++ src/packages/layout/ALayout.vue
@@ -3,6 +3,7 @@
     ref="containerRef"
     :class="{
       'a-layout': true,
+      'a-layout--fill': shouldFillParent,
       'a-layout--has-side': hasSide,
     }"
   >
@@ -11,18 +12,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, provide } from 'vue';
+import { defineComponent, ref, provide, onMounted } from 'vue';
+
+const RELATIVE_POSITION_KEYS = ['relative', 'absolute', 'fixed'];
 
 export default defineComponent({
   setup() {
     const hasSide = ref(false);
     const containerRef = ref<HTMLElement | undefined>();
 
+    const shouldFillParent = ref(false);
+
     provide('layout', { hasSide });
+
+    onMounted(() => {
+      if (!containerRef.value) {
+        return;
+      }
+      const parentEl = containerRef.value.parentElement;
+      if (!parentEl) {
+        return;
+      }
+      if (RELATIVE_POSITION_KEYS.includes(getComputedStyle(parentEl).position || '')) {
+        shouldFillParent.value = true;
+      }
+    });
 
     return {
       hasSide,
       containerRef,
+      shouldFillParent,
     };
   },
 });
@@ -37,6 +56,11 @@ export default defineComponent({
   flex-direction: column;
   box-sizing: border-box;
 }
+
+.a-layout--fill {
+  height: 100%;
+}
+
 .a-layout--has-side {
   flex-direction: row;
 }
