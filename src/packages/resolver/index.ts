82f110ffb14db26ede56140052b0bec79c32c0e2
BackRunner <dev@backrunner.top>
2023-11-15 02:45:20 +0800

    fix: (resolver) import related component style automatically

--- src/packages/resolver/index.ts
+++ src/packages/resolver/index.ts
@@ -16,6 +16,15 @@ const NATIVE_NAMES = [
   'AbsoluteOrientationSensor',
 ];
 
+// some component contains generated ui, it will directly use some other component
+const STYLE_LINKED_COMPONENT_MAP = {
+  checkboxGroup: 'checkbox',
+  radioGroup: 'radio',
+  radioButtonGroup: 'radioButton',
+  listMenu: 'listMenuItem',
+  popMenu: 'popper',
+};
+
 const NO_STYLE_COMPONENTS = ['checkboxGroup', 'virtualListItem'];
 
 export interface AnyUIResolverOptions {
@@ -24,18 +33,38 @@ export interface AnyUIResolverOptions {
   blacklist?: NameBlacklist;
 }
 
+const getStyleImport = (name: string, styleType: string) =>
+  `@any-design/anyui/styles/components/${name}.${styleType}`;
+
 const getSideEffects = (importName: string, options: AnyUIResolverOptions) => {
   const { styleType = 'css', importStyle = true } = options;
   if (!importStyle) {
     return;
   }
+
+  const sideEffects: string[] = [];
+
   // generate style path
   const formattedName = importName[1].toLowerCase() + importName.slice(2);
+
   // check no style components
   if (NO_STYLE_COMPONENTS.includes(formattedName)) {
     return;
   }
-  return `@any-design/anyui/styles/components/${formattedName}.${styleType}`;
+
+  sideEffects.push(getStyleImport(formattedName, styleType));
+
+  if (Object.keys(STYLE_LINKED_COMPONENT_MAP).includes(formattedName)) {
+    const relatedComps =
+      STYLE_LINKED_COMPONENT_MAP[formattedName as keyof typeof STYLE_LINKED_COMPONENT_MAP];
+    if (Array.isArray(relatedComps)) {
+      sideEffects.push(...relatedComps.map((compName) => getStyleImport(compName, styleType)));
+    } else if (typeof relatedComps === 'string') {
+      sideEffects.push(getStyleImport(relatedComps, styleType));
+    }
+  }
+
+  return sideEffects;
 };
 
 const isNameExcluded = (name: string, blacklist?: NameBlacklist) => {
